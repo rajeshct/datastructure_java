53aeaf4643d64bd8f5eda3f42905d268da51d272
Rajesh Tiwari <rajeshchandratiwari@gmail.com>
2019-11-03 23:49:30 +0530

    Create LinkedListOperation.java
    Created LinkedList operation:
    - Create LinkedList 
    - Reverse LinkedList.
    - Print LinkedList.

---
+++ LinkedListOperation.java
@@ -0,0 +1,59 @@
+package com.remember.password;
+
+public class LinkedListOperation {
+
+    public static void main(String args[]) {
+        Node head = new Node(1, new Node());
+        createList(head);
+        head = reverseList(head);
+        printList(head);
+    }
+
+    private static Node reverseList(Node head) {
+        Node current = head;
+        Node previous = null;
+        Node next;
+        
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+
+
+    private static void createList(Node head) {
+        Node current = head.next;
+        for (int i = 2; i < 20; i++) {
+            current.value = i;
+            current.next = new Node();
+            current = current.next;
+        }
+        current.value = 20;
+    }
+
+    private static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+}
+
+
+class Node {
+    public int value;
+    Node next;
+
+    Node() {
+    }
+
+    Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+}
