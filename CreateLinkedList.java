71e5ca80ced10e675225edec3a0792d80d726310
Rajesh Tiwari <rajeshchandratiwari@gmail.com>
2019-11-04 01:30:04 +0530

    Update CreateLinkedList.java
    Updated create list code

--- CreateLinkedList.java
+++ CreateLinkedList.java
@@ -3,19 +3,18 @@ package com.remember.password;
 public class CreateLinkedList {
 
     public static void main(String args[]) {
-        Node head = new Node(1, new Node());
+        Node head = new Node(1, null);
         createList(head);
         printList(head);
     }
 
     private static void createList(Node head) {
-        Node current = head.next;
-        for (int i = 2; i < 20; i++) {
-            current.value = i;
-            current.next = new Node();
-            current = current.next;
+        Node current=head;
+        for(int i=2;i<20;i++){
+            current.next =new Node();
+            current=current.next;
+            current.value=i;
         }
-        current.value = 20;
     }
 
     private static void printList(Node head) {
