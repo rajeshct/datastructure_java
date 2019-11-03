8a9d021f2a6ce2a55fdca15eadd9cefa5ee37258
Rajesh Tiwari <rajeshchandratiwari@gmail.com>
2019-11-04 01:11:24 +0530

    Update ReverseListFromGivenNode.java
    Updated comments

--- ReverseListFromGivenNode.java
+++ ReverseListFromGivenNode.java
@@ -12,7 +12,10 @@ public class ReverseListFromGivenNode {
     private static void reverseListFromPosition(Node head,Node reverseElement){
         Node temp=head;
         Node lastNode=null;
-        
+        /* 
+        * As in linked-list their is only one way traversing i.e in forward direction
+        * So currently tracking the previous node from reverse element
+        */
         while(temp!=null){
             if(temp.next==reverseElement){
                 lastNode= temp;
@@ -21,6 +24,15 @@ public class ReverseListFromGivenNode {
             temp=temp.next;
         }
         
+        /*
+        * Process for reversing list is simple just make start node next address to null
+        * and all the connecting node so on will contain index of start node like
+        *  [1,100](address 500) -> [2,101](address 100) -> [3,102](address 101) -> [4,103] (address 102) -> [5,104] (address 103)
+        *  -> [6,NULL] (address 104)
+        *
+        *  [6,109] (address 104) -> [5,106] (address 103)-> [4,103] (address 102)-> [3,100] (address 101) -> [2,500] (address 100) 
+        * -> [1,NULL] (address 500)
+        */
         Node current =reverseElement;
         Node previous=null;
         Node next=null;
